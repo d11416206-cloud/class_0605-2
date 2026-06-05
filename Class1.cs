4177865f60b2c07ca14dbdce44dd35bc26ee8f16
d11416206-cloud <d11416206@gs.takming.edu.tw>
2026-06-05 12:07:49 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public abstract class Person
+{
+	public abstract string GetName();
+	
+}
+public class Student : Person
+{
+	public override string GetName()
+	{
+		return "Student";
+	}
+}
+
+public class Teacher : Person
+{
+	public override string GetName()
+	{
+		return "Teacher";
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace class_0605_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Student();
+            Console.WriteLine(p1.GetName());
+            p1 = new Teacher();
+            Console.WriteLine(p1.GetName());
+            string input =Console.ReadLine();
+            if (input =="1")
+            {
+                p1 = new Student();
+            }
+            else
+            {
+                p1 = new Teacher();
+            }
+            Console.WriteLine(p1.GetName());
+        }
+    }
+}
